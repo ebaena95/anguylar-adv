ee262afd33aa605ebd7f9ab882da7d52aa84d6aa
Eva Baena <ebaena.dam@institutcampalans.net>
2022-07-14 16:04:35 +0200

    moduls de pages i shared

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Graphic1Component } from './graphic1/graphic1.component';
+import { ProgressComponent } from './progress/progress.component';
+import { PagesComponent } from './pages.component';
+import { SharedModule } from '../shared/shared.module';
+import { AppRoutingModule } from '../routes/app-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Graphic1Component,
+    PagesComponent
+
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    AppRoutingModule
+    
+  ],
+  exports:[
+    DashboardComponent,
+    ProgressComponent,
+    Graphic1Component,
+    PagesComponent
+
+  ]
+})
+export class PagesModule { }
